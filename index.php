3bdee347720ce487a7c675c0148b94171b6cbf55
William Camargo <william_galdino@econeteditora.com.br>
2021-08-31 13:00:03 -0300

    contro

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
-use \App\controller\Pages\Home;
-$home = new Home();
-echo $home->getHome();
+use \App\Controller\Pages\Home;
+
+echo Home::getHome();
